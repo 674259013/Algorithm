ee415fa510ba421fc2572cf07bc3598d008a85d9
674259013 <naruapon@gmail.com>
2024-07-30 10:40:23 +0700

    Algo2

---
+++ asdfsfs.java
@@ -0,0 +1,10 @@
+public class asdfsfs {
+    static int num1 = 1;
+    static int num2 = 6;
+    static int sum = num1+num2;
+public static void main(String arg[]){
+    System.out.println(sum);
+
+}
+
+}
