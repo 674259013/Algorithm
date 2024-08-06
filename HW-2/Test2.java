f56ebe95724ffa2b6847d17f13dd0758937efb43
674259013 <674259013@webmail.npru.ac.th>
2024-08-06 12:14:52 +0700

    cel and scan

---
+++ HW-2/Test2.java
@@ -0,0 +1,25 @@
+public class Test2 {
+    public static void main (String args[]){
+        byte b = 1; //8
+        short s = 2; //16
+        int i = 3; //32
+        long l = 4; //64
+
+        float f = 5.45f; //32
+        double d = 6; //64
+
+        char c = 'C'; //16
+
+        boolean bool = true; //null
+
+        System.out.println(b) ;
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bool);
+        
+    }
+}
---
+++ TestGO/go.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	name := "watcharakorn"
+	fmt.Println(name)
+
+	var fahrenheit float32 = 80
+    var celsius float32 = (fahrenheit-80)*5/9
+
+	fmt.Println(celsius)
+
+}
+ 
